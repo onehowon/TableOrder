5ba3d891bd45044fbcde5bff1fa57475b9cdda21
onehowon <flash0221@naver.com>
2025-05-01 14:20:25 +0900

    add: 프로젝트 추가

---
+++ src/test/java/com/ebiz/tableorder/TableorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebiz.tableorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
