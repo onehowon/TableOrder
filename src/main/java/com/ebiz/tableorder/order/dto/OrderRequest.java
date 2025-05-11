6135159657ee0e3f7fde3beada101e6b73f27e87
OneHoWon <flash0221@ajou.ac.kr>
2025-05-11 16:18:15 +0900

    feat: 공통 timezone 설정

--- src/main/java/com/ebiz/tableorder/order/dto/OrderRequest.java
+++ src/main/java/com/ebiz/tableorder/order/dto/OrderRequest.java
@@ -1,19 +1,25 @@
 package com.ebiz.tableorder.order.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 
 import java.util.List;
 
 @Getter
 @Builder
+@Jacksonized
 public class OrderRequest {
     private final int tableNumber;
     private final List<OrderItemReq> items;
 
-    @Getter @Builder
+    @Getter
+    @Builder
+    @Jacksonized
     public static class OrderItemReq {
         private final Long menuId;
-        private final int  quantity;
+        private final int quantity;
     }
 }
