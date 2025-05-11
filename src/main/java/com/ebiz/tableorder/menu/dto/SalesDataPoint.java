19a5bba8fa5d973462b8a98221e6e3c884214e04
OneHoWon <flash0221@ajou.ac.kr>
2025-05-11 14:01:14 +0900

    fix: 생성자 시그니처 변경

--- src/main/java/com/ebiz/tableorder/menu/dto/SalesDataPoint.java
+++ src/main/java/com/ebiz/tableorder/menu/dto/SalesDataPoint.java
@@ -1,4 +1,16 @@
 package com.ebiz.tableorder.menu.dto;
 
 
-public record SalesDataPoint(int hour, long revenue) {}
+import lombok.Getter;
+
+@Getter
+public class SalesDataPoint {
+    private final Integer hour;
+    private final Long revenue;
+
+    // JPQL new 구문과 완전히 일치하도록 반드시 선언해야 합니다.
+    public SalesDataPoint(Integer hour, Long revenue) {
+        this.hour = hour;
+        this.revenue = revenue;
+    }
+}
