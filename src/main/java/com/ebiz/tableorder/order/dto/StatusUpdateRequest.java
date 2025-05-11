b6c8a96c536faee52e28d8c2c33e6c3950dc5944
OneHoWon <flash0221@ajou.ac.kr>
2025-05-11 20:49:37 +0900

    fix: 역직렬화 문제 해결

--- src/main/java/com/ebiz/tableorder/order/dto/StatusUpdateRequest.java
+++ src/main/java/com/ebiz/tableorder/order/dto/StatusUpdateRequest.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class StatusUpdateRequest {
     @NotBlank
     private String status;
