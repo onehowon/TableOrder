d8eede481b49e3edc635e574b3da93c45b889c54
OneHoWon <flash0221@ajou.ac.kr>
2025-05-11 19:07:49 +0900

    feat: 공통 timezone 설정

--- src/main/java/com/ebiz/tableorder/TableorderApplication.java
+++ src/main/java/com/ebiz/tableorder/TableorderApplication.java
@@ -3,10 +3,15 @@ package com.ebiz.tableorder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TableorderApplication {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
 		SpringApplication.run(TableorderApplication.class, args);
 	}
 
